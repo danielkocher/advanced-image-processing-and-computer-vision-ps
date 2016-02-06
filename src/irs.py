e482fb0c516a048b3872ac4f5fd31668a67d9dab
Daniel Kocher <Daniel.Kocher@stud.sbg.ac.at>
2016-02-06 12:01:51 +0100

    Implement file reads

--- src/irs.py
+++ src/irs.py
@@ -3,7 +3,6 @@
 # Advanced Image Processing & Computer Vision class at University of Salzburg.
 # 
 # Author: Daniel Kocher
-#
 ################################################################################
 
 ################################################################################
@@ -13,7 +12,8 @@
 import sys
 import os
 import numpy
-import sklearn
+#import sklearn
+import cv2
 
 import settings.settings as settings
 
@@ -24,6 +24,25 @@ def main():
     settings.print_settings()
   except NameError as ne:
     print('NameError: {0}'.format(ne))
+
+  attributes = read_file(settings.filepaths['attributes'])
+  images = read_file(settings.filepaths['images'])
+
+  votes_tmp = read_file(settings.filepaths['votes'])
+  votes = split_entries(votes_tmp, ' ')
+
+def read_file(path):
+  content = []
+  with open(path, 'r') as f:
+    for line in f:
+      content.append(line.strip())
+  return content
+
+def split_entries(l, delimiter):
+  l_new = []
+  for entry in l:
+    l_new.append([float(i) for i in entry.split()])
+  return l_new
 
 if __name__ == "__main__":
   main()
